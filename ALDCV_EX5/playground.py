6bdd5bf1372b169db1c5533541858d34090b1e72
Dan <750866996@qq.com>
2024-03-04 16:20:18 +0100

    Update playground.py

--- ALDCV_EX5/playground.py
+++ ALDCV_EX5/playground.py
@@ -173,7 +173,7 @@ embedder = Embedder(d_input, n_freqs, log_space)
 
 points = pts.reshape((-1, 3))
 # idxs =  torch.linspace(0, pts.shape[0], steps=8, dtype=torch.int)
-idxs = torch.linspace(0, pts.shape[0] - 1, steps=8, dtype=torch.long)
+idxs = torch.linspace(0, pts.shape[0] - 1, steps=8, dtype=torch.float).long()
 points = points[idxs]
 embeddings = embedder(points).cpu().detach().numpy()
 
